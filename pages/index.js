72f2410536ceacff7f87bfa2e3eeacc16d022291
khosrora <khosrora153333@gmail.com>
2022-11-08 09:20:20 +0330

    change style home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Layout from '../components/public/layout';
 import HomePage from "../components/public/HomePage";
-
+import { wrapper } from "./../redux/store";
 
 
 const Home = () => {
@@ -10,6 +10,10 @@ const Home = () => {
     <>
       <Head>
         <title>کافه ایران | menu Digital | منو دیجیتال کافه ها و رستوران ها</title>
+        <meta property="url" content="cafesiran.ir" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="fa_IR" />
+        <meta property="og:site_name" content="کافه ایران" />
         <meta name="description" content="کافه ایران ارائه دهنده سرویس منو دیجیتال به کافه ها و رستوران ها و آبمیوه بستنی ها و هتل ها |‌کافه ایران" />
       </Head>
       <Layout>
@@ -21,3 +25,11 @@ const Home = () => {
 
 
 export default Home;
+
+Home.getInitialProps = wrapper.getInitialPageProps((store) => async () => {
+  try {
+
+  } catch (error) {
+    console.log(error);
+  }
+});
