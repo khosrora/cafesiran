4880d6c1838f5da8104af955fd0165a792a14ef5
khosrora <khosrora153333@gmail.com>
2022-11-08 09:00:27 +0330

    change style home page

--- components/public/HomePage.js
+++ components/public/HomePage.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { SearchIcon, CheckIcon, ArrowLeftIcon } from '@heroicons/react/outline'
 import Link from 'next/link';
@@ -38,79 +38,178 @@ const HomePage = () => {
         }
     }
 
+    //! Install pwa
+    const [supportsPWA, setSupportsPWA] = useState(false);
+    const [promptInstall, setPromptInstall] = useState(null);
+
+    useEffect(() => {
+        const handler = e => {
+            e.preventDefault();
+            console.log("we are being triggered :D");
+            setSupportsPWA(true);
+            setPromptInstall(e);
+        };
+        window.addEventListener("beforeinstallprompt", handler);
+
+        return () => window.removeEventListener("transitionend", handler);
+    }, []);
+
+    const onClick = e => {
+        e.preventDefault();
+        if (!promptInstall) {
+            return;
+        }
+        promptInstall.prompt();
+    };
+    if (!supportsPWA) {
+        return null;
+    }
+
     return (
-        <div className="w-full mt-20 mb-40 flex flex-col justify-center items-center lg:mt-44">
-            <h1 className="text-6xl font-bold">کافه <span className="text-amber-600">ایران</span></h1>
-            <div className="mt-8 font-bold">منو دیجیتال برای کافه و رستوران و ... شما</div>
-            <div className="px-4 mt-8 text-[10px] flex flex-col justify-start items-start gap-y-4 lg:text-sm">
-                <div className="flex justify-start items-center">
-                    <CheckIcon className='w-4 h-4 ml-2' />
-                    <span className="">تعامل پویا با مشتری برای کسب و کار شما </span>
-                    <br />
-                </div>
-                <div className="flex justify-start items-center">
-                    <CheckIcon className='w-4 h-4 ml-2' />
-                    <span className="">دسترسی سریع مشتریان به منو ( کد کافه و qrcode و نقشه و ... )</span>
-                    <br />
-                </div>
-                <div className="flex justify-start items-center">
-                    <CheckIcon className='w-4 h-4 ml-2' />
-                    <span className=""> قابلیت ایجاد سطح دسترسی برای سالن داران </span>
-                    <br />
-                </div>
-                <div className="flex justify-start items-center">
-                    <CheckIcon className='w-4 h-4 ml-2' />
-                    <span className=""> نمایش گزارشات گرافیکی از سفارشات ثبت شده</span>
-                    <br />
-                </div>
-                <div className="flex justify-start items-center">
-                    <CheckIcon className='w-4 h-4 ml-2' />
-                    <span className=""> ثبت مجموعه شما در کمتر از 3 دقیقه </span>
-                    <br />
-                </div>
-                <Link href="/features">
-                    <a className="flex justify-start items-center">
-                        <ArrowLeftIcon className='w-4 h-4 ml-2' />
-                        <span className=""> مشاهده توضیحات بیشتر ... </span>
-                        <br />
-                    </a>
-                </Link>
-            </div>
-            <div className="flex flex-col gap-y-2 lg:flex lg:flex-row mt-4">
-                <div className="mb-4 md:mb-0 relative">
-                    <input onChange={(e) => setSearchBtn(e.target.value)} onKeyDown={handleKeyDown} type="text" className="border-2 border-slate-600 rounded-md p-2 px-7 dark:border-none dark:outline-none w-full" placeholder="جست وجو بر اساس کد کافه" />
-                </div>
-                {
-                    lengthSearch == 5 ?
-                        load ?
-                            <div className="border-2 bg-slate-900 text-white rounded-md p-2 px-7 flex justify-between items-center mr-0 md:mr-2 dark:bg-amber-600 dark:border-none">
-                                <p>لطفا منتظر بمانید</p>
-                            </div> :
-                            <div onClick={() => handleSearchCafe()} className="border-2 bg-slate-900 text-white rounded-md p-2 px-7 flex justify-between items-center cursor-pointer mr-0 lg:mr-2 dark:bg-amber-600 dark:border-none">
-                                <p>
-                                    جست وجو کن
-                                </p>
+        <>
+            <div className="hidden w-full my-8 lg:flex">
+                <div className="w-full max-w-7xl m-auto">
+                    <div className="bg-zinc-100 flex rounded-lg justify-between items-center py-8 dark:bg-zinc-900">
+                        <div className="w-1/2 h-full px-8">
+                            <h1 className="text-4xl font-extrabold">کافه <span className="text-amber-600">ایران</span></h1>
+                            <div className="mt-8 font-bold text-sm">منو دیجیتال برای کافه و رستوران و ... شما</div>
+                            <div className="mt-8 text-[10px] flex flex-col justify-start items-start gap-y-4 lg:text-sm">
+                                <div className="flex justify-start items-center">
+                                    <CheckIcon className='w-4 h-4 ml-2' />
+                                    <span className="">تعامل پویا با مشتری برای کسب و کار شما </span>
+                                    <br />
+                                </div>
+                                <div className="flex justify-start items-center">
+                                    <CheckIcon className='w-4 h-4 ml-2' />
+                                    <span className="">دسترسی سریع مشتریان به منو ( کد کافه و qrcode و نقشه و ... )</span>
+                                    <br />
+                                </div>
+                                <div className="flex justify-start items-center">
+                                    <CheckIcon className='w-4 h-4 ml-2' />
+                                    <span className=""> قابلیت ایجاد سطح دسترسی برای سالن داران </span>
+                                    <br />
+                                </div>
+                                <div className="flex justify-start items-center">
+                                    <CheckIcon className='w-4 h-4 ml-2' />
+                                    <span className=""> نمایش گزارشات گرافیکی از سفارشات ثبت شده</span>
+                                    <br />
+                                </div>
+                                <div className="flex justify-start items-center">
+                                    <CheckIcon className='w-4 h-4 ml-2' />
+                                    <span className=""> ثبت مجموعه شما در کمتر از 3 دقیقه </span>
+                                    <br />
+                                </div>
+                                <Link href="/features">
+                                    <a className="flex justify-start items-center">
+                                        <ArrowLeftIcon className='w-4 h-4 ml-2' />
+                                        <span className=""> مشاهده توضیحات بیشتر ... </span>
+                                        <br />
+                                    </a>
+                                </Link>
                             </div>
-                        :
-                        <div onClick={() => SetProvinceMenu(!provinceMenu)} className="border-2 bg-slate-900 rounded-md p-2 px-7 flex justify-between items-center cursor-pointer mr-0 md:mr-2 dark:bg-amber-600 dark:border-none">
-                            <p className="text-white">جستجو بر اساس استان</p>
-                            <SearchIcon className="h-4 w-4 mr-10 text-white" />
                         </div>
-                }
-                <Link href="/qrCodeReader">
-                    <div className="border-2 bg-slate-900 rounded-md p-2 px-7 flex justify-between items-center cursor-pointer dark:bg-amber-600 dark:border-none lg:hidden">
-                        <p className="text-white">بارکد خوان</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-4 w-4 mr-10 text-white">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 013.75 9.375v-4.5zM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 01-1.125-1.125v-4.5zM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0113.5 9.375v-4.5z" />
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75zM6.75 16.5h.75v.75h-.75v-.75zM16.5 6.75h.75v.75h-.75v-.75zM13.5 13.5h.75v.75h-.75v-.75zM13.5 19.5h.75v.75h-.75v-.75zM19.5 13.5h.75v.75h-.75v-.75zM19.5 19.5h.75v.75h-.75v-.75zM16.5 16.5h.75v.75h-.75v-.75z" />
-                        </svg>
+                        <div className="w-1/2 flex justify-end items-center">
+                            <img className='w-96' src="/images/features/Group527.png" alt="DigitalMenuImage" />
+                        </div>
                     </div>
-                </Link>
+                    <div className="w-10/12 bg-white border rounded-md m-auto -mt-10 dark:bg-zinc-800 dark:border-none">
+                        <div className="flex justify-center items-center gap-x-4 w-11/12 m-auto py-2">
+                            <div className="relative border rounded-md flex justify-center items-center px-4 w-2/4 dark:bg-zinc-700 dark:border-none">
+                                <input onChange={(e) => setSearchBtn(e.target.value)} onKeyDown={handleKeyDown} className='text-xs py-4 w-3/4 focus:outline-none dark:bg-zinc-700' type="text" name="" id="" placeholder='جست و جو با کد کافه' />
+                                <div className="w-1/4 text-center flex justify-center items-center">
+                                    {
+                                        lengthSearch == 5 ? <span onClick={() => handleSearchCafe()} className='text-sm cursor-pointer bg-amber-600 p-1 rounded-full text-white w-full'> جست و جو کن  </span> :
+                                            <SearchIcon className='w-4 h-4 absolute left-5' />
+                                    }
+                                </div>
+                            </div>
+                            <div className="border rounded-md py-4 px-4 w-1/4 dark:bg-zinc-700 cursor-pointer dark:border-none" onClick={() => SetProvinceMenu(!provinceMenu)}>
+                                <div className="flex justify-between items-center w-full cursor-pointer" onClick={() => SetProvinceMenu(!provinceMenu)}>
+                                    <p className='text-xs'>جستجو بر اساس استان</p>
+                                    <ArrowLeftIcon className='w-4 h-4 left-0' />
+                                </div>
+                            </div>
+                            <div className="w-1/4 py-4 rounded-md" onClick={(e) => onClick(e)}>
+                                <button className='bg-amber-600 w-full rounded-md text-white py-4 text-sm'>نصب وب اپلیکیشن</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <div className="w-full mt-20 mb-40 flex flex-col justify-center items-center lg:hidden">
+                <h1 className="text-6xl font-bold">کافه <span className="text-amber-600">ایران</span></h1>
+                <div className="mt-8 font-bold">منو دیجیتال برای کافه و رستوران و ... شما</div>
+                <div className="px-4 mt-8 text-[10px] flex flex-col justify-start items-start gap-y-4 lg:text-sm">
+                    <div className="flex justify-start items-center">
+                        <CheckIcon className='w-4 h-4 ml-2' />
+                        <span className="">تعامل پویا با مشتری برای کسب و کار شما </span>
+                        <br />
+                    </div>
+                    <div className="flex justify-start items-center">
+                        <CheckIcon className='w-4 h-4 ml-2' />
+                        <span className="">دسترسی سریع مشتریان به منو ( کد کافه و qrcode و نقشه و ... )</span>
+                        <br />
+                    </div>
+                    <div className="flex justify-start items-center">
+                        <CheckIcon className='w-4 h-4 ml-2' />
+                        <span className=""> قابلیت ایجاد سطح دسترسی برای سالن داران </span>
+                        <br />
+                    </div>
+                    <div className="flex justify-start items-center">
+                        <CheckIcon className='w-4 h-4 ml-2' />
+                        <span className=""> نمایش گزارشات گرافیکی از سفارشات ثبت شده</span>
+                        <br />
+                    </div>
+                    <div className="flex justify-start items-center">
+                        <CheckIcon className='w-4 h-4 ml-2' />
+                        <span className=""> ثبت مجموعه شما در کمتر از 3 دقیقه </span>
+                        <br />
+                    </div>
+                    <Link href="/features">
+                        <a className="flex justify-start items-center">
+                            <ArrowLeftIcon className='w-4 h-4 ml-2' />
+                            <span className=""> مشاهده توضیحات بیشتر ... </span>
+                            <br />
+                        </a>
+                    </Link>
+                </div>
+                <div className="flex flex-col gap-y-2 lg:flex lg:flex-row mt-4">
+                    <div className="mb-4 md:mb-0 relative">
+                        <input onChange={(e) => setSearchBtn(e.target.value)} onKeyDown={handleKeyDown} type="text" className="border-2 border-slate-600 rounded-md p-2 px-7 dark:border-none dark:outline-none w-full" placeholder="جست وجو بر اساس کد کافه" />
+                    </div>
+                    {
+                        lengthSearch == 5 ?
+                            load ?
+                                <div className="border-2 bg-slate-900 text-white rounded-md p-2 px-7 flex justify-between items-center mr-0 md:mr-2 dark:bg-amber-600 dark:border-none">
+                                    <p>لطفا منتظر بمانید</p>
+                                </div> :
+                                <div onClick={() => handleSearchCafe()} className="border-2 bg-slate-900 text-white rounded-md p-2 px-7 flex justify-between items-center cursor-pointer mr-0 lg:mr-2 dark:bg-amber-600 dark:border-none">
+                                    <p>
+                                        جست وجو کن
+                                    </p>
+                                </div>
+                            :
+                            <div onClick={() => SetProvinceMenu(!provinceMenu)} className="border-2 bg-slate-900 rounded-md p-2 px-7 flex justify-between items-center cursor-pointer mr-0 md:mr-2 dark:bg-amber-600 dark:border-none">
+                                <p className="text-white">جستجو بر اساس استان</p>
+                                <SearchIcon className="h-4 w-4 mr-10 text-white" />
+                            </div>
+                    }
+                    <Link href="/qrCodeReader">
+                        <div className="border-2 bg-slate-900 rounded-md p-2 px-7 flex justify-between items-center cursor-pointer dark:bg-amber-600 dark:border-none lg:hidden">
+                            <p className="text-white">بارکد خوان</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-4 w-4 mr-10 text-white">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 013.75 9.375v-4.5zM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 01-1.125-1.125v-4.5zM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0113.5 9.375v-4.5z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75zM6.75 16.5h.75v.75h-.75v-.75zM16.5 6.75h.75v.75h-.75v-.75zM13.5 13.5h.75v.75h-.75v-.75zM13.5 19.5h.75v.75h-.75v-.75zM19.5 13.5h.75v.75h-.75v-.75zM19.5 19.5h.75v.75h-.75v-.75zM16.5 16.5h.75v.75h-.75v-.75z" />
+                            </svg>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+
             {
                 provinceMenu ? <ProvinceMenu SetProvinceMenu={SetProvinceMenu} /> : null
             }
-        </div>
+        </>
     );
 }
 
--- components/shared/modals/ProvinceMenu.js
+++ components/shared/modals/ProvinceMenu.js
@@ -1,29 +1,31 @@
 import Link from "next/link";
 import { province } from './../utilities/constance/province';
-import {XIcon} from '@heroicons/react/outline';
+import { XIcon } from '@heroicons/react/outline';
 
 
 
 const ProvinceMenu = ({ SetProvinceMenu }) => {
     return (
         <>
-            <div onClick={() => SetProvinceMenu(false)} className="fixed bg-slate-900 opacity-50 top-0 right-0 left-0 bottom-0 z-50"></div>
-            <div className="fixed bg-slate-50 w-80 h-auto -mt-20 rounded-md md:w-3/6 md:h-3/6 overflow-y-auto dark:bg-zinc-900 z-50">
-                <div className="px-8 mt-4">
-                    <div className="flex justify-between items-center">
-                        <p className="text-sm">استان مورد نظر خود را انتخاب کنید</p>
-                        <XIcon onClick={() => SetProvinceMenu(false)}  className="h-6 w-6 cursor-pointer" />
+            <div className="absolute top-0 right-0 left-0 bottom-0 flex flex-col justify-center items-center">
+                <div onClick={() => SetProvinceMenu(false)} className="fixed bg-slate-900 opacity-50 top-0 right-0 left-0 bottom-0 z-50"></div>
+                <div className="fixed bg-slate-50 w-80 h-auto -mt-20 rounded-md md:w-3/6 md:h-3/6 overflow-y-auto dark:bg-zinc-900 z-50">
+                    <div className="px-8 mt-4">
+                        <div className="flex justify-between items-center">
+                            <p className="text-sm">استان مورد نظر خود را انتخاب کنید</p>
+                            <XIcon onClick={() => SetProvinceMenu(false)} className="h-6 w-6 cursor-pointer" />
+                        </div>
                     </div>
-                </div>
-                <div className="px-8 mt-4 mb-2">
-                    <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
-                        {
-                            province.map(i =>
-                                <Link key={i.pk} href={`cafes?province=${i.fields.slug}&&id=${i.pk}`}>
-                                    <p className="bg-slate-100 p-2 rounded  text-xs text-center cursor-pointer dark:bg-zinc-800 ">{i.fields.name}</p>
-                                </Link>
-                            )
-                        }
+                    <div className="px-8 mt-4 mb-2">
+                        <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
+                            {
+                                province.map(i =>
+                                    <Link key={i.pk} href={`cafes?province=${i.fields.slug}&&id=${i.pk}`}>
+                                        <p className="bg-slate-100 p-2 rounded  text-xs text-center cursor-pointer dark:bg-zinc-800 ">{i.fields.name}</p>
+                                    </Link>
+                                )
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
