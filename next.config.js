818cb649b13067bbbb50c68245a13571dae0c2ed
khosrora <khosrora153333@gmail.com>
2023-02-03 11:55:00 +0330

    fixed sw

--- next.config.js
+++ next.config.js
@@ -12,20 +12,33 @@
 //   }
 // });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  compress: false,
-  poweredByHeader: false,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== 'development',
-  },
-};
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
 });
 
+const nextConfig = withPWA({
+  reactStrictMode: true,
+});
+module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   compress: false,
+//   poweredByHeader: false,
+//   swcMinify: true,
+//   compiler: {
+//     removeConsole: process.env.NODE_ENV !== 'development',
+//   },
+// };
+
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   disable: process.env.NODE_ENV === 'development',
+//   register: true,
+// });
