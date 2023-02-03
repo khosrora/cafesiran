f4647f7674b3dafa18b1e6c608e97f42ee02d59e
khosrora <khosrora153333@gmail.com>
2023-02-03 12:05:57 +0330

    fixed sw

--- next.config.js
+++ next.config.js
@@ -12,33 +12,36 @@
 //   }
 // });
 
-const runtimeCaching = require("next-pwa/cache");
-const withPWA = require("next-pwa")({
-  dest: "public",
+// const runtimeCaching = require("next-pwa/cache");
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+//   runtimeCaching,
+//   buildExcludes: [/middleware-manifest.json$/],
+// });
+
+// const nextConfig = withPWA({
+//   reactStrictMode: true,
+// });
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  buildExcludes: [/middleware-manifest.json$/],
 });
 
 const nextConfig = withPWA({
   reactStrictMode: true,
+  compress: false,
+  poweredByHeader: false,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
 });
+
 module.exports = nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   compress: false,
-//   poweredByHeader: false,
-//   swcMinify: true,
-//   compiler: {
-//     removeConsole: process.env.NODE_ENV !== 'development',
-//   },
-// };
-
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   disable: process.env.NODE_ENV === 'development',
-//   register: true,
-// });
