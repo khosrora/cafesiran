b72bd56a1a274e06be404001885c81f94d2898e9
khosrora <khosrora153333@gmail.com>
2023-02-03 12:39:17 +0330

    fixed sw

--- next.config.js
+++ next.config.js
@@ -1,67 +1,18 @@
-const runtimeCaching = require("next-pwa/cache");
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  buildExcludes: [/middleware-manifest.json$/],
-});
-
-const nextConfig = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  compress: false,
+  poweredByHeader: false,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  }
+};
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
 });
-module.exports = nextConfig;
 
-// const withPWA = require("next-pwa");
-
-
-
-// module.exports = withPWA({
-//   reactStrictMode: true,
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     disable: process.env.NODE_ENV === 'development',
-//     skipWaiting: true,
-//   }
-// });
-
-// /** @type {import('next').NextConfig} */
-
-// const runtimeCaching = require("next-pwa/cache");
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   runtimeCaching , 
-//   skipWaiting: true,
-//   disable: process.env.NODE_ENV === 'development',
-//   register: true,
-// });
-
-// module.exports = nextConfig = withPWA({
-//   reactStrictMode: true,
-//   compress: false,
-//   poweredByHeader: false,
-//   swcMinify: true,
-//   compiler: {
-//     removeConsole: process.env.NODE_ENV !== 'development',
-//   },
-// });
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   compress: false,
-//   poweredByHeader: false,
-//   swcMinify: true,
-//   compiler: {
-//     removeConsole: process.env.NODE_ENV !== 'development',
-//   },
-// };
-
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   disable: process.env.NODE_ENV === 'development',
-//   register: true,
-// });
-
-// module.exports = withPWA(nextConfig);
+module.exports = withPWA(nextConfig);
