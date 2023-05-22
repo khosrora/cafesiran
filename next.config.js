b277bacab69623c13d47ff275c11ae8ca3780ca3
khosro rasouli <khosrora@khosros-MacBook-Air.local>
2023-05-22 08:57:48 +0330

    fixed pwa

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-// const withPWA = require("next-pwa");
+const withPWA = require("next-pwa");
 
 
 
-// module.exports = withPWA({
-//   reactStrictMode: true,
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     disable: process.env.NODE_ENV === 'development',
-//     skipWaiting: true,
-//   }
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    disable: process.env.NODE_ENV === 'development',
+    skipWaiting: true,
+  }
 
-// });
+});
 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  /* config options here */
-}
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   /* config options here */
+// }
 
 module.exports = nextConfig
